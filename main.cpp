4a8cacf1c1236fd99805eba3cce7398614545ef2
krzysztow <krzysztow@gmail.com>
2021-03-01 13:59:53 +0000

    Poking into WASM memory

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 extern "C"
-int add (int first, int second)
-{
+int add (int first, int second) {
   return first + second;
+}
+
+extern "C"
+void unsafe_memcpy(const char* in, long long size, char* out) {
+  for (int i = 0; i < size; ++i) {
+    out[i] = in[i];
+  }
 }
--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+set -x
+
 /emsdk/upstream/bin/clang++ --target=wasm32 --no-standard-libraries -Wl,--export-all -Wl,--no-entry -o add.wasm main.cpp
